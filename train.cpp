0f39c52eee0a57da7f79d322c09758810416e8a6
pranavrajesh23 <pranav.rajesh@psiog.com>
2025-08-17 12:35:12 +0530

    ticket collection metho

--- train.cpp
+++ train.cpp
@@ -1,6 +1,22 @@
 #include<iostream>
 using namespace std;
 
+void collectTicket()
+{
+    cout<<"\n--------Ticket Collection--------"<<endl;
+    cout<<"\nHow do you want to collect your ticket? \n1.Mail\n2.Ticket Machine \n ";
+    int tic;
+    cin >> tic;
+    if(tic==1)
+    {
+        cout<<"\nCheck your letter box in 3 days for the ticket\n";
+    }
+    else
+    {
+        cout<<"\nCollect your ticket from the ticket machine at the station\n";
+    }
+}
+
 void reservation()
 {
     cout<<"\n--------Train Reservation--------"<<endl;
@@ -14,7 +30,7 @@ void reservation()
     {
         cout << "\nNo reservation needed.\n";
     }
-    // collectTicket();
+    collectTicket();
 }
 
 
